bbcddf5c6e3dab8a68372b6d1e0560bc0bd92044
Frederic Jarjour <91092194+fredjarjour@users.noreply.github.com>
2024-08-09 18:17:42 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -58,7 +58,7 @@ mm_5l_sc = [
 ];
 
 mm_fl_sc = [
-    ["Stascious", "35.396", "https://www.youtube.com/watch?v=3OwcpmREiBk"],
+    ["Stascious", "34.745", "https://youtu.be/8zSDnKhZY-E"],
     ["MyntiFresh", "34.801", "https://www.youtube.com/watch?v=v46CsZ8RoXE"]
 ];
 
