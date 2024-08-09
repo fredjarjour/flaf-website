f14b68c5b79efb75e751e42283ffc0ba7f0e66d3
Frederic Jarjour <frederic.jarjour@umontreal.ca>
2024-08-08 20:45:45 -0400

    added ultra shortcuts

--- docs/script.js
+++ docs/script.js
@@ -37,6 +37,23 @@ fh_fl_sc = [
     return timeA - timeB;
 });
 
+fh_5l_us = [
+
+].sort((a, b) => {
+    const timeA = timeStringToSeconds(a[1]);
+    const timeB = timeStringToSeconds(b[1]);
+    return timeA - timeB;
+});
+
+fh_fl_us = [
+    
+].sort((a, b) => {
+    const timeA = timeStringToSeconds(a[1]);
+    const timeB = timeStringToSeconds(b[1]);
+    return timeA - timeB;
+});
+
+
 mm_5l_ns = [
 
 ].sort((a, b) => {
@@ -69,6 +86,23 @@ mm_fl_sc = [
     return timeA - timeB;
 });
 
+mm_5l_us = [
+
+].sort((a, b) => {
+    const timeA = timeStringToSeconds(a[1]);
+    const timeB = timeStringToSeconds(b[1]);
+    return timeA - timeB;
+});
+
+mm_fl_us = [
+        
+].sort((a, b) => {
+    const timeA = timeStringToSeconds(a[1]);
+    const timeB = timeStringToSeconds(b[1]);
+    return timeA - timeB;
+});
+
+
 sh_5l_ns = [
 
 ].sort((a, b) => {
@@ -86,7 +120,7 @@ sh_fl_ns = [
 });
 
 sh_5l_sc = [
-
+    
 ].sort((a, b) => {
     const timeA = timeStringToSeconds(a[1]);
     const timeB = timeStringToSeconds(b[1]);
@@ -95,6 +129,22 @@ sh_5l_sc = [
 
 sh_fl_sc = [
     
+].sort((a, b) => {
+    const timeA = timeStringToSeconds(a[1]);
+    const timeB = timeStringToSeconds(b[1]);
+    return timeA - timeB;
+});
+
+sh_5l_us = [
+    
+].sort((a, b) => {
+    const timeA = timeStringToSeconds(a[1]);
+    const timeB = timeStringToSeconds(b[1]);
+    return timeA - timeB;
+});
+
+sh_fl_us = [
+        
 ].sort((a, b) => {
     const timeA = timeStringToSeconds(a[1]);
     const timeB = timeStringToSeconds(b[1]);
@@ -112,7 +162,7 @@ function fh_load() {
 
     document.getElementById("fh-ns-5l").innerHTML = ih;
 
-    ih = "<h2>Fastest Lap - No Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    ih = "<h2>Flap - No Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
     for (i = 0; i < fh_fl_ns.length; i++) {
         ih += "<tr><td><a href='" + fh_fl_ns[i][2] + "'>" + (i + 1) + ". " + fh_fl_ns[i][0] + "</a></td><td><a href='" + fh_fl_ns[i][2] + "'>" + fh_fl_ns[i][1] + "</a></td></tr>";
     }
@@ -128,13 +178,29 @@ function fh_load() {
 
     document.getElementById("fh-sc-5l").innerHTML = ih;
 
-    ih = "<h2>Fastest Lap - Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    ih = "<h2>Flap - Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
     for (i = 0; i < fh_fl_sc.length; i++) {
         ih += "<tr><td><a href='" + fh_fl_sc[i][2] + "'>" + (i + 1) + ". " + fh_fl_sc[i][0] + "</a></td><td><a href='" + fh_fl_sc[i][2] + "'>" + fh_fl_sc[i][1] + "</a></td></tr>";
     }
     ih += "</table>";
 
     document.getElementById("fh-sc-fl").innerHTML = ih;
+
+    ih = "<h2>5lap - Ultra Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    for (i = 0; i < fh_5l_us.length; i++) {
+        ih += "<tr><td><a href='" + fh_5l_us[i][2] + "'>" + (i + 1) + ". " + fh_5l_us[i][0] + "</a></td><td><a href='" + fh_5l_us[i][2] + "'>" + fh_5l_us[i][1] + "</a></td></tr>";
+    }
+    ih += "</table>";
+
+    document.getElementById("fh-us-5l").innerHTML = ih;
+
+    ih = "<h2>Flap - Ultra Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    for (i = 0; i < fh_fl_us.length; i++) {
+        ih += "<tr><td><a href='" + fh_fl_us[i][2] + "'>" + (i + 1) + ". " + fh_fl_us[i][0] + "</a></td><td><a href='" + fh_fl_us[i][2] + "'>" + fh_fl_us[i][1] + "</a></td></tr>";
+    }
+    ih += "</table>";
+
+    document.getElementById("fh-us-fl").innerHTML = ih;
 }
 
 function mm_load() {
@@ -146,7 +212,7 @@ function mm_load() {
 
     document.getElementById("mm-ns-5l").innerHTML = ih;
 
-    ih = "<h2>Fastest Lap - No Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    ih = "<h2>Flap - No Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
     for (i = 0; i < mm_fl_ns.length; i++) {
         ih += "<tr><td><a href='" + mm_fl_ns[i][2] + "'>" + (i + 1) + ". " + mm_fl_ns[i][0] + "</a></td><td><a href='" + mm_fl_ns[i][2] + "'>" + mm_fl_ns[i][1] + "</a></td></tr>";
     }
@@ -162,13 +228,29 @@ function mm_load() {
 
     document.getElementById("mm-sc-5l").innerHTML = ih;
 
-    ih = "<h2>Fastest Lap - Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    ih = "<h2>Flap - Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
     for (i = 0; i < mm_fl_sc.length; i++) {
         ih += "<tr><td><a href='" + mm_fl_sc[i][2] + "'>" + (i + 1) + ". " + mm_fl_sc[i][0] + "</a></td><td><a href='" + mm_fl_sc[i][2] + "'>" + mm_fl_sc[i][1] + "</a></td></tr>";
     }
     ih += "</table>";
 
     document.getElementById("mm-sc-fl").innerHTML = ih;
+
+    ih = "<h2>5lap - Ultra Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    for (i = 0; i < mm_5l_us.length; i++) {
+        ih += "<tr><td><a href='" + mm_5l_us[i][2] + "'>" + (i + 1) + ". " + mm_5l_us[i][0] + "</a></td><td><a href='" + mm_5l_us[i][2] + "'>" + mm_5l_us[i][1] + "</a></td></tr>";
+    }
+    ih += "</table>";
+
+    document.getElementById("mm-us-5l").innerHTML = ih;
+
+    ih = "<h2>Flap - Ultra Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    for (i = 0; i < mm_fl_us.length; i++) {
+        ih += "<tr><td><a href='" + mm_fl_us[i][2] + "'>" + (i + 1) + ". " + mm_fl_us[i][0] + "</a></td><td><a href='" + mm_fl_us[i][2] + "'>" + mm_fl_us[i][1] + "</a></td></tr>";
+    }
+    ih += "</table>";
+
+    document.getElementById("mm-us-fl").innerHTML = ih;
 }
 
 function sh_load() {
@@ -180,7 +262,7 @@ function sh_load() {
 
     document.getElementById("sh-ns-5l").innerHTML = ih;
 
-    ih = "<h2>Fastest Lap - No Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    ih = "<h2>Flap - No Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
     for (i = 0; i < sh_fl_ns.length; i++) {
         ih += "<tr><td><a href='" + sh_fl_ns[i][2] + "'>" + (i + 1) + ". " + sh_fl_ns[i][0] + "</a></td><td><a href='" + sh_fl_ns[i][2] + "'>" + sh_fl_ns[i][1] + "</a></td></tr>";
     }
@@ -196,11 +278,27 @@ function sh_load() {
 
     document.getElementById("sh-sc-5l").innerHTML = ih;
 
-    ih = "<h2>Fastest Lap - Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    ih = "<h2>Flap - Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
     for (i = 0; i < sh_fl_sc.length; i++) {
         ih += "<tr><td><a href='" + sh_fl_sc[i][2] + "'>" + (i + 1) + ". " + sh_fl_sc[i][0] + "</a></td><td><a href='" + sh_fl_sc[i][2] + "'>" + sh_fl_sc[i][1] + "</a></td></tr>";
     }
     ih += "</table>";
 
     document.getElementById("sh-sc-fl").innerHTML = ih;
+
+    ih = "<h2>5lap - Ultra Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    for (i = 0; i < sh_5l_us.length; i++) {
+        ih += "<tr><td><a href='" + sh_5l_us[i][2] + "'>" + (i + 1) + ". " + sh_5l_us[i][0] + "</a></td><td><a href='" + sh_5l_us[i][2] + "'>" + sh_5l_us[i][1] + "</a></td></tr>";
+    }
+    ih += "</table>";
+
+    document.getElementById("sh-us-5l").innerHTML = ih;
+
+    ih = "<h2>Flap - Ultra Shortcut</h2><table><tr><th>Player</th><th>Time</th></tr>";
+    for (i = 0; i < sh_fl_us.length; i++) {
+        ih += "<tr><td><a href='" + sh_fl_us[i][2] + "'>" + (i + 1) + ". " + sh_fl_us[i][0] + "</a></td><td><a href='" + sh_fl_us[i][2] + "'>" + sh_fl_us[i][1] + "</a></td></tr>";
+    }
+    ih += "</table>";
+
+    document.getElementById("sh-us-fl").innerHTML = ih;
 }
