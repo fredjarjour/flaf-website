1927440baa4248a4bae695052ed7db4024bacc48
Frederic Jarjour <frederic.jarjour@umontreal.ca>
2024-09-03 14:53:20 -0400

    update

--- docs/script.js
+++ docs/script.js
@@ -206,7 +206,7 @@ mm_beta.categories.get("5lap - Shortcut").push(...[
 ]);
 
 mm_beta.categories.get("Flap - Shortcut").push(...[
-    
+    ["MyntiFresh", "00:32.717", "https://www.youtube.com/watch?v=s4SFQVI3VWg"]
 ]);
 
 mm_beta.categories.get("5lap - Ultra Shortcut").push(...[
@@ -214,7 +214,7 @@ mm_beta.categories.get("5lap - Ultra Shortcut").push(...[
 ]);
 
 mm_beta.categories.get("Flap - Ultra Shortcut").push(...[
-    ["MyntiFresh", "00:32.717", "https://www.youtube.com/watch?v=s4SFQVI3VWg"]
+    
 ]);
 //#endregion Midnight Motorist
 
