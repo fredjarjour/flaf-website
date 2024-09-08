f1368bac4039cfe0b0d403476e195111631fbf45
Frederic Jarjour <frederic.jarjour@umontreal.ca>
2024-09-07 23:05:24 -0400

    update

--- docs/script.js
+++ docs/script.js
@@ -221,7 +221,7 @@ mm_beta.categories.get("Flap - Ultra Shortcut").push(...[
 
 //#region Sinkhole
 sh_beta.categories.get("5lap - No Shortcut").push(...[
-    ["Rinify", "2:52.000", "https://www.youtube.com/watch?v=UJEIc9YIpUg"]
+    ["Rinify", "2:49.067", "https://www.youtube.com/watch?v=-eab8pW1zTw"]
 ]);
 
 sh_beta.categories.get("Flap - No Shortcut").push(...[
